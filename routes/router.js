c39e96637331f0d51e68b53e80e3bc8aa4c6f760
brandaorafael <brandao.rafael@hotmail.com>
2015-11-08 05:07:56 -0200

    Add funcionalidade check

--- routes/router.js
+++ routes/router.js
@@ -4,41 +4,69 @@ module.exports = function(app){
 
 
 	app.get('/check', function(req, res){
+		
+		if(req.query.mesa != null){
+			var query = {
+				mesa: req.query.mesa
+			}
 
-		mongo.connect('mongodb://localhost:27017/site', function(err, db) {
-			db.collection('pedidos').findOne(function(err, doc) {
-        		if(err) throw err;
+			mongo.connect('mongodb://localhost:27017/site', function(err, db) {
+				db.collection('pedidos').find(query).toArray(function(err, doc){
+					if (err) throw err;
 
-        		if (!doc) {
-		            console.dir("No document found");
-		            return db.close();
-		        }
-		        return db.close();
-		    });
-		});
+					console.dir(doc);
+					res.json(doc);
+					return db.close();
+				});
+			});
+		} else {
+			var query = {}
 
+			mongo.connect('mongodb://localhost:27017/site', function(err, db) {
+				db.collection('pedidos').find(query).sort({'hora':1}).toArray(function(err, doc){
+					if (err) throw err;
+
+					var mesa = [];
+
+					for(var i = 0; i < doc.length; i++){
+						if(!doc[i].finalizado){
+							mesa.push(doc[i].mesa);
+						}
+					}
+
+					console.dir(mesa);
+					res.json(mesa);
+					return db.close();
+				});
+			});
+		}	
 	});
 
 
 	app.get('/pedido', function(req, res){
 
 		var query = {
-			// mesa: req.query.mesa
+			mesa: req.query.mesa
 		}
 
 		var update = {
+
+			mesa: req.query.mesa,
+
 			pedido: [
 				{
 					// 'chop': req.body.chop
-					'chop': 0
+					'chop': 1
 				},{
 					// 'refri': req.body.refri
-					'refri': 0
+					'refri': 2
 				},{
 					// 'agua': req.body.agua
 					'agua': 0
 				}
 			],
+
+			hora: new Date(),
 
 			finalizado: false
 		}
