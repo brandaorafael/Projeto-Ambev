20fc860633f888831887e72aa4079b98f40494b9
Rafael Brandão <Rafael@MacBook-Pro-de-Rafael-2.local>
2016-01-17 21:43:30 -0200

    Nao lembro o que essas mudancas fazem

--- routes/router.js
+++ routes/router.js
@@ -55,8 +55,17 @@ module.exports = function(app){
 			mesa: req.query.mesa
 		}
 
+		var update = {
+			chop: 0,
+			refri: 0,
+			agua: 0,
+			mesa: req.query.mesa,
+			hora: new Date(),
+			finalizado: true
+		}
+
 		mongo.connect(mongoURI, function(err, db) {
-			db.collection('pedidos').remove(query, function(err, data){
+			db.collection('pedidos').update(query, update, function(err, data){
 				if (err) throw err;
 
 		        console.log("Pedido entregue");
